b0423973e4172670e225b1970c0123a1d8060b0f
meghavi <mjadav@parkar.digital>
2025-01-29 18:17:54 +0530

    Preparing base template

--- frontend/src/Components/DashBoard/DashBoard.js
+++ frontend/src/Components/DashBoard/DashBoard.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import Slider from '../Slider/Slider.js'
+import CityList from '../CityList/CityList.js'
+import WikiPage from '../WikiPage/WikiPage.js'
+import SearchBar from '../SearchBar/SearchBar.js'
+import './DashBoard.css'
 
 export default function DashBoard() {
   return (
-    <div>DashBoard</div>
+    <div className='main-container' >      
+      <div className='slider-component' id='slider'>
+        <Slider />
+      </div>
+      <div className='citylist-component' id='citylist'>
+        <CityList />
+      </div>
+      <div className='wikipage-component' id='wikipage'>
+        <WikiPage />
+      </div>
+      <div className='searchbar-component' id='searchbar'>
+        <SearchBar />
+      </div>
+    </div>
   )
 }
---
+++ frontend/src/Components/WikiPage/WikiPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function WikiPage(){
+  return (
+    <div>WikiPage</div>
+  )
+}
